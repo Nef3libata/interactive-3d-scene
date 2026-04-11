fa1c6148a23ec6444793daab145faef0807a4bac
samaneh <samanehcosmonaut@gmail.com>
2026-04-11 08:27:22 +0200

    chore: add App component layout with Header and LeftSidebar structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import { Header } from "./components/Header/Header";
+import { LeftSidebar } from "./components/LeftSidebar/LeftSidebar";
+import "./App.scss";
 
 function App() {
   return (
-    <div>
+    <div className="app">
       <Header />
+      <div className="app__content">
+        <LeftSidebar />
+      </div>
     </div>
   );
 }
