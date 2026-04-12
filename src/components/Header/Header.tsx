008182391ae31f78cd56471323fb84b670d7754e
samaneh <samanehcosmonaut@gmail.com>
2026-04-12 23:55:45 +0200

    chore: add help button to Header with dialog toggle and improve logo accessibility

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,36 @@
+import { useState } from "react";
 import logoIcon from "@components/icons/logo-icon.svg";
+import { HelpIcon } from "@components/icons/HelpIcon";
+import { HelpDialog } from "../HelpDialog/HelpDialog";
 import "./Header.scss";
 
 export const Header = () => {
+  const [helpOpen, setHelpOpen] = useState(false);
+
   return (
-    <header className="header">
-      <div className="header__left">
-        <div className="header__logo">
-          <div className="header__logo-icon">
-            <img src={logoIcon} />
+    <>
+      <header className="header">
+        <div className="header__left">
+          <div className="header__logo">
+            <div className="header__logo-icon">
+              <img src={logoIcon} alt="logo" />
+            </div>
+            <span className="header__title">3D Viewer</span>
           </div>
-          <span className="header__title">3D Viewer</span>
         </div>
-      </div>
-    </header>
+
+        <div className="header__right">
+          <button
+            className="header__help-btn"
+            onClick={() => setHelpOpen(true)}
+            aria-label="Help"
+          >
+            <HelpIcon />
+          </button>
+        </div>
+      </header>
+
+      {helpOpen && <HelpDialog onClose={() => setHelpOpen(false)} />}
+    </>
   );
 };
