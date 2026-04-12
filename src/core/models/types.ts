80c67452fbf62efe98c83291f0c404cc146739ae
samaneh <samanehcosmonaut@gmail.com>
2026-04-12 18:17:29 +0200

    chore: simplify Sphere interface by replacing Vector3 with tuple, removing name, adding visible property, and removing SphereFormData

--- src/core/models/types.ts
+++ src/core/models/types.ts
@@ -1,16 +1,9 @@
-import { Vector3 } from "three";
-
 export interface Sphere {
   id: string;
-  position: Vector3;
+  position: [number, number, number];
   radius: number;
   color: string;
-  name: string;
-}
-
-export interface SphereFormData {
-  radius: number;
-  color: string;
+  visible: boolean;
 }
 
 export type ViewPreset = "top" | "front" | "side" | "reset";
