a2692bc1047b290c5d7612f341c5012831524595
samaneh <samanehcosmonaut@gmail.com>
2026-04-13 11:10:21 +0200

    chore: remove visible property from Sphere interface

--- src/core/models/types.ts
+++ src/core/models/types.ts
@@ -3,7 +3,6 @@ export interface Sphere {
   position: [number, number, number];
   radius: number;
   color: string;
-  visible: boolean;
 }
 
 export type ViewPreset = "top" | "front" | "side" | "reset";
